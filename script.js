28a6b5be454e73d1ab8c79746bbcea3f36c7f809
wifon <kaziorigor0902@gmail.com>
2024-01-22 14:30:38 +0100

    feat: adding minutes

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const startButton = document.querySelector(".start");
 const stopButton = document.querySelector(".stop");
 const resetButton = document.querySelector(".reset");
 
+const appendMinutes = document.querySelector(".minutes");
 const appendSeconds = document.querySelector(".seconds");
 const appendTens = document.querySelector(".tens");
 
 let interval;
+let minutes = 0;
 let seconds = 0;
 let tens = 0;
 
@@ -43,6 +45,18 @@ function startTimer() {
   if (seconds > 9) {
     appendSeconds.innerHTML = seconds;
   }
+
+  if (seconds >= 60) {
+    minutes++;
+    appendMinutes.innerHTML = "0" + minutes;
+
+    seconds = 0;
+    appendSeconds.innerHTML = seconds;
+  }
+
+  if (minutes > 9) {
+    appendMinutes.innerHTML = minutes;
+  }
 }
 
 function stopTimer() {
@@ -51,8 +65,10 @@ function stopTimer() {
 
 function resetTimer() {
   stopTimer();
+  minutes = 0;
   seconds = 0;
   tens = 0;
+  appendMinutes.innerHTML = "00";
   appendSeconds.innerHTML = "00";
   appendTens.innerHTML = "00";
 }
